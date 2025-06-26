404047a0c322ac1a9ed9a261a30a0d41405c6e07
Ibrahim Ben <ib117@student.london.ac.uk>
2025-06-26 19:38:35 +0200

    added take health damage method to player

--- NinjaBrainstorm/Assets/Scripts/PlayerController.cs
+++ NinjaBrainstorm/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,21 @@ public class PlayerController : MonoBehaviour
     public float  maxHealth = 100f;
     public float  currentHealth;
 
+    [SerializeField] bool DEBUG_takeHealthDamage = false;
+
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (DEBUG_takeHealthDamage)
+        {
+            DEBUG_takeHealthDamage = false;
+            TakeHealthDamage(5);
+            UIManager.Instance.SetHealth(playerName, currentHealth);
+        }
     }
 
     public void StartTurn()
@@ -21,5 +33,11 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log($"{playerName}'s turn ended.");
         // Disable input, etc.
+    }
+
+    public void TakeHealthDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        currentHealth = Mathf.Max(currentHealth, 0);
     }
 }
